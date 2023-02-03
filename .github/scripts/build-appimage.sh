1abd384d83bf8a9f79e758d4961905da5952a5db
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-03 17:10:28 +0100

    Use non-dev sdl package

--- .github/scripts/build-appimage.sh
+++ .github/scripts/build-appimage.sh
@@ -40,19 +40,18 @@ echo "Installing dependencies into appdir"
 echo "Installing non python dependencies"
 # xclip is needed for Kivy on X11 systems
 # libsdl2-image-dev is not preinstalled on some systems, i.e. Pop!_OS 22.04
-apt-get download xclip libsdl2-image-dev
+apt-get download xclip libsdl2-image-2.0-0
 # extract debs
 mkdir /tmp/xclip
 dpkg-deb -R ./xclip*.deb /tmp/xclip
 mkdir /tmp/libsdl2
-dpkg-deb -R ./libsdl2-image-dev*.deb /tmp/libsdl2
+dpkg-deb -R ./libsdl2-image-2.0-0*.deb /tmp/libsdl2
 rm *.deb # remove debs
 # copy binaries into appdir
 # Xclip
 cp -r /tmp/xclip/usr/bin /tmp/kivy_appdir/usr
 # SDL2
-cp -r /tmp/libsdl2/usr/include /tmp/kivy_appdir/usr
-cp -r /tmp/libsdl2/usr/lib/x86_64-linux-gnu/libSDL2_image* /tmp/kivy_appdir/usr/lib
+cp -r /tmp/libsdl2/usr/lib/x86_64-linux-gnu/* /tmp/kivy_appdir/usr/lib
 # Clean appdir
 echo "Uninstalling unneeded python dependencies from appdir"
 /tmp/kivy_appdir/AppRun -m pip uninstall -y wheel build setuptools urllib3 tomli pyproject_hooks pkg_resources packaging idna charset_normalizer certifi
